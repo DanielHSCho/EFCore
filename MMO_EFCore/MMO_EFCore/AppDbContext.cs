feb09f36a03a6cccc7308485314150f566d65ad4
daniel <hellena171791@gmail.com>
2022-04-19 13:31:47 +0900

    <EF - DB연동> EFCore가 DB 세팅할 수 있도록 DbSet 선언 (+)
    - EFCore 동작 스텝
    1) DBContext 만들때 DbSet<T>을 찾는다
    2) T형식을 분석한다 (모델링 클래스를 분석해 컬럼을 찾음)
    3) 분석중에 외부 참조가 있다면 개네도 같이 분석한다
    4) 분석이 끝나면 OnModelCreating 함수를 호출한다 (추가 설정이 필요한 경우 이 함수를 오버라이딩한다)
    5) 최종적으로 DB 전체 모델링 구조를 내부 메모리에 들고 있어서 굳이 DBcontext를 다시 분석하지 않도록 한다

---
+++ MMO_EFCore/MMO_EFCore/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MMO_EFCore
+{
+    public class AppDbContext : DbContext
+    {
+        // DbSet<Item> -> EFCore한테 알려준다
+        public DbSet<Item> Items { get; set; }
+    }
+}
