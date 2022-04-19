35ec1eb7731a612a13aba9a979d74a0ffa782d0b
daniel <hellena171791@gmail.com>
2022-04-19 13:42:54 +0900

    <EF - Db연동> OnConfiguring 옵션 설정 / ConnectionString 연결

--- MMO_EFCore/MMO_EFCore/AppDbContext.cs
+++ MMO_EFCore/MMO_EFCore/AppDbContext.cs
@@ -9,5 +9,16 @@ namespace MMO_EFCore
     {
         // DbSet<Item> -> EFCore한테 알려준다
         public DbSet<Item> Items { get; set; }
+
+        // DB ConString - 어떤 DB를 어떻게 연결해라
+        // (각종 설정, Authorization 등 포함)
+        // TODO : 보안상 외부 Config파일로 빼야한다
+        public const string ConnectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EfCoreDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        // 처음 DB 연동 부분을 옵션으로 넣는다
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(ConnectionString);
+        }
     }
 }
