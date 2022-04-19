da6ad5910e6bd1c4520de9fdf7e223f4433f9cc3
daniel <hellena171791@gmail.com>
2022-04-19 13:55:41 +0900

    <EF - DB연동> Table이름이 Items로 생성되지 않게 직접 명시

--- MMO_EFCore/MMO_EFCore/DataModel.cs
+++ MMO_EFCore/MMO_EFCore/DataModel.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace MMO_EFCore
 {
+    [Table("Item")]
     public class Item
     {
         // 이름Id -> PK
