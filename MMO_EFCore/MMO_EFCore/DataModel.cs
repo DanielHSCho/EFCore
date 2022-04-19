b44d19a857d061aef761f7acd794d112ef7ac3d2
daniel <hellena171791@gmail.com>
2022-04-19 13:13:52 +0900

    <EF - DB연동> 데이터 모델링 (+)
    - Item과 Player 클래스 생성 = 테이블 생성
    - 프라이머리 키 지정
    - Item이 Owner로 Player 클래스 참조하도록 함 (Navigation 프로퍼티라고 하며, 컬럼으로 별도 존재하진 않고 외부 참조를 위해 사용됨)

---
+++ MMO_EFCore/MMO_EFCore/DataModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MMO_EFCore
+{
+    public class Item
+    {
+        // 이름Id -> PK
+        public int ItemId { get; set; }
+        public int TemplateId { get; set; } // 101번 = 집행검
+        public DateTime CreateDate { get; set; }
+
+        // 다른 클래스 참조 -> FK (Navigational Property)
+        public int OwnerId { get; set; }
+        public Player Owner { get; set; }
+    }
+
+    // 클래스 이름 = 테이블 이름 = Player
+    public class Player
+    {
+        // 이름Id -> PK
+        public int PlayerId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
