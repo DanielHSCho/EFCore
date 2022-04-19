53b756f61c1ae54016552d7d5723d4d366dabc47
daniel <hellena171791@gmail.com>
2022-04-19 13:50:42 +0900

    <EF - DB연동> DB 생성 함수 추가

--- MMO_EFCore/MMO_EFCore/Program.cs
+++ MMO_EFCore/MMO_EFCore/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using System;
 
 namespace MMO_EFCore
 {
     class Program
     {
+        static void InitializeDB(bool forceReset = false)
+        {
+            // DB 사용 후 날려주는 경우가 일반적이므로 using 방식으로 사용
+            using (AppDbContext db = new AppDbContext()){
+                if(!forceReset && (db.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists()) {
+                    return;
+                }
+
+                db.Database.EnsureDeleted();
+                // DB가 존재하지 않으면 삭제하고 생성
+                db.Database.EnsureCreated();
+
+                Console.WriteLine("DB Initialized");
+            } ;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            InitializeDB(forceReset: true);
         }
     }
 }
