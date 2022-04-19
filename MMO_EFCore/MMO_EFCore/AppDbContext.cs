3316da7d638e7668da6a3361be749ef35ffb4ec0
daniel <hellena171791@gmail.com>
2022-04-19 13:55:13 +0900

    <EF - DB연동> 골뱅이 추가(백슬래시 두번 쓰거나)

--- MMO_EFCore/MMO_EFCore/AppDbContext.cs
+++ MMO_EFCore/MMO_EFCore/AppDbContext.cs
@@ -13,7 +13,7 @@ namespace MMO_EFCore
         // DB ConString - 어떤 DB를 어떻게 연결해라
         // (각종 설정, Authorization 등 포함)
         // TODO : 보안상 외부 Config파일로 빼야한다
-        public const string ConnectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EfCoreDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public const string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=EfCoreDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         // 처음 DB 연동 부분을 옵션으로 넣는다
         protected override void OnConfiguring(DbContextOptionsBuilder options)
