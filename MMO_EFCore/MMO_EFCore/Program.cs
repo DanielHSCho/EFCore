7d1b45ffb276b0bc6f15bea1f915147bab3d5cda
daniel <hellena171791@gmail.com>
2022-04-20 12:52:35 +0900

    <EF - CRUD> DB 프로그램화

--- MMO_EFCore/MMO_EFCore/Program.cs
+++ MMO_EFCore/MMO_EFCore/Program.cs
@@ -7,6 +7,28 @@ namespace MMO_EFCore
         static void Main(string[] args)
         {
             DbCommands.InitializeDB(forceReset: false);
+
+            Console.WriteLine("명령어를 입력하세요");
+            Console.WriteLine("[0] Force Reset");
+            Console.WriteLine("[1] Read All");
+
+            while (true) {
+                Console.Write("> ");
+                string command = Console.ReadLine();
+                switch (command) {
+                    case "0":
+                        DbCommands.InitializeDB(forceReset: false);
+                        break;
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                }
+
+            }
+
         }
     }
 }
