bbbe5480787fb625de4c0aedfca8a6a7b5e3db6e
daniel <hellena171791@gmail.com>
2022-04-21 12:58:21 +0900

    <EF - 연결/비연결> Update Reload / Full 방식 비교 (+)
    - Reload방식 : 필요 정보만 보내서 3스텝 밟음
    - Full방식 : 모든 정보를 다보내고 받아서 아예 새로운 Entity를 통으로 만듬

--- MMO_EFCore/MMO_EFCore/Program.cs
+++ MMO_EFCore/MMO_EFCore/Program.cs
@@ -10,6 +10,8 @@ namespace MMO_EFCore
 
             Console.WriteLine("명령어를 입력하세요");
             Console.WriteLine("[0] Force Reset");
+            Console.WriteLine("[1] Update (Reload)");
+            Console.WriteLine("[2] Update (Full)");
 
             while (true) {
                 Console.Write("> ");
@@ -19,9 +21,10 @@ namespace MMO_EFCore
                         DbCommands.InitializeDB(forceReset: true);
                         break;
                     case "1":
-                        DbCommands.UpdateTest();
+                        DbCommands.UpdateByReload();
                         break;
                     case "2":
+                        DbCommands.UpdateByFull();
                         break;
                     case "3":
                         break;
